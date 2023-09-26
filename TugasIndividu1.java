4cb92523674e6e3aca9c472057bd2acda47d95d9
Naufal Gajendra Ardiputra <53498520+naufal025@users.noreply.github.com>
2023-09-26 16:43:24 +0700

    Add files via upload

---
+++ TugasIndividu1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class TugasIndividu1{
+public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    int jarak;
+
+    System.out.print("Masukkan jarak anda: ");
+    jarak = sc.nextInt();
+    if(jarak <= 5){
+    System.out.println("Gunakan melee weapon");
+    } else if (jarak > 5){
+    System.out.println("Gunakan ranged weapon");
+    }
+
+   }
+
+}
+   
